3928358e97c5ee950164f43480166c78c3ddb0e2
BavarescoM <mauricio.bavaresco@unochapeco.edu.br>
2020-04-04 04:04:52 -0300

    cont 17

---
+++ backend/src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: "e9bd8235e775b2dc838ecbdc3c28b78b",
+  expiresIn: "7d",
+};
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,8 +1,12 @@
 import { Router } from "express";
 import Usercontroller from "./app/controllers/UserController";
-
+import Sessioncontroller from "./app/controllers/SessionController";
+import authMiddleware from "./app/middlewares/auth";
 const routes = new Router();
 
 routes.post("/users", Usercontroller.store);
+routes.post("/sessions", Sessioncontroller.store);
+routes.use(authMiddleware);
+routes.put("/users", Usercontroller.update);
 
 export default routes;
