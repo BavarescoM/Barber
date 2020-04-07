d4d0923e740e70689cd0ac1f17f62da5ac078ea1
BavarescoM <mauric222838@br-atacadao.corp>
2020-04-07 20:50:32 -0300

    continuar 13 e configurar mongo

--- backend/src/database/index.js
+++ backend/src/database/index.js
@@ -3,18 +3,28 @@ import User from "../app/models/User";
 import File from "../app/models/File";
 import Appointment from "../app/models/Appointment";
 import databaseConfig from "../config/database";
-
+import mongoose from 'mongoose';
 const models = [User, File, Appointment];
 
 class Database {
   constructor() {
     this.init();
+    this.mongo();
   }
   init() {
     this.connection = new Sequelize(databaseConfig);
     models
       .map(model => model.init(this.connection))
       .map(model => model.associate && model.associate(this.connection.models));
+  }
+  mongo() {
+    this.mongoConnection = mongoose.connect(
+      'mongodb://localhost:27017/gobarber', 
+      {
+        useNewUrlParser: true, useFindAndModify: true
+      }
+    )
+
   }
 }
 export default new Database();
--- backend/src/routes.js
+++ backend/src/routes.js
@@ -3,11 +3,13 @@ import UserController from "./app/controllers/UserController";
 import SessionController from "./app/controllers/SessionController";
 import FileController from "./app/controllers/FileController";
 import ProviderController from "./app/controllers/ProviderController";
+import NotificationController from "./app/controllers/NotificationController";
 
 import authMiddleware from "./app/middlewares/auth";
 import multer from "multer";
 import multerConfig from "./config/multer";
-import AppoinmentController from "./app/controllers/AppoinmentController";
+import AppoinmentController from "./app/controllers/AppointmentController";
+import ScheduleController from "./app/controllers/ScheduleController";
 
 const routes = new Router();
 const upload = multer(multerConfig);
@@ -20,7 +22,11 @@ routes.get("/providers", ProviderController.index);
 routes.post("/files", upload.single("file"), FileController.store);
 
 routes.post("/Appointments", AppoinmentController.store);
+routes.get("/Appointments", AppoinmentController.index);
 
 routes.put("/users", UserController.update);
+routes.get('/schedule', ScheduleController.index);
+routes.get('/notifications', NotificationController.index);
+
 
 export default routes;
